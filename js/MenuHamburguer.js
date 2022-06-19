8802896553b9f6702b30d4bd63286962eab3618c
João Vitor Rodrigues de souza <jv479439@gmail.com>
2022-06-18 23:27:22 -0300

    adding Hamburguer Menu

---
+++ js/MenuHamburguer.js
@@ -0,0 +1,10 @@
+
+const OpenMenuHamburguer = () => {
+    const OpenMenuHamburguer = document.querySelector('#MenuHamburguer')
+    OpenMenuHamburguer.classList.toggle('hidden')
+}
+
+const CloseMenuHamburguer = () => {
+    const CloseMenuHamburguer = document.querySelector('#MenuHamburguer')
+    CloseMenuHamburguer.classList.toggle('hidden')
+}
