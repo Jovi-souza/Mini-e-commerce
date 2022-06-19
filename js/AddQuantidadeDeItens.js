82b6ad6eff92820bedf2f0bc1f3a8fdfe258ceca
João Vitor Rodrigues de souza <jv479439@gmail.com>
2022-06-19 13:29:12 -0300

    Update Items

--- js/AddQuantidadeDeItens.js
+++ js/AddQuantidadeDeItens.js
@@ -4,16 +4,24 @@ const QuantityOfItems = () => {
     return document.querySelector('#QuantityOfItens')
 }
 
+const ItensInMyCart = () => {
+    return document.querySelector('#ItensInMyCart')
+}
+
 const AddItens = () => {
     valor++
     QuantityOfItems().innerHTML = valor
+    ItensInMyCart().innerHTML = valor
 }
 
 const RemoveItens = () => {
     valor--
-    QuantityOfItems().innerHTML = valor
-
-    if( valor <= 0 ) {
-        valor = 1
+    
+    if (valor <= -1) {
+        return valor = 0
     }
+
+    QuantityOfItems().innerHTML = valor
+    ItensInMyCart().innerHTML = valor
+
 }
