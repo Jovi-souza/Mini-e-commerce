7020b91ccbd314a577945f6e9aa3e34be9b412ab
João Vitor Rodrigues de souza <jv479439@gmail.com>
2022-06-18 23:35:50 -0300

    Optimizing code

--- js/MenuHamburguer.js
+++ js/MenuHamburguer.js
@@ -1,10 +1,3 @@
-
-const OpenMenuHamburguer = () => {
-    const OpenMenuHamburguer = document.querySelector('#MenuHamburguer')
-    OpenMenuHamburguer.classList.toggle('hidden')
-}
-
-const CloseMenuHamburguer = () => {
-    const CloseMenuHamburguer = document.querySelector('#MenuHamburguer')
-    CloseMenuHamburguer.classList.toggle('hidden')
+const OpenCloseMenuHamburguer = () => {
+    return document.querySelector('#MenuHamburguer').classList.toggle('hidden')
 }
