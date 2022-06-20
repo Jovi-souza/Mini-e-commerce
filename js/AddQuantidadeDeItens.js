ffa4818d851e891d9eb883e5314ba39ae21f9e60
João Vitor Rodrigues de souza <jv479439@gmail.com>
2022-06-19 22:03:13 -0300

    Improving the code

--- js/AddQuantidadeDeItens.js
+++ js/AddQuantidadeDeItens.js
@@ -1,27 +1,26 @@
 let valor = 0
 
-const QuantityOfItems = () => {
-    return document.querySelector('#QuantityOfItens')
-}
 
-const ItensInMyCart = () => {
-    return document.querySelector('#ItensInMyCart')
+const DOMSelectors = {
+    NumberOfItens: document.querySelector('#NumberOfItens'),
+    QuantityOfItems: document.querySelector('#QuantityOfItens'),
+    NumberOfItensInMyCart: document.querySelector('#NumberOfItensInMyCart')
 }
 
 const AddItens = () => {
     valor++
-    QuantityOfItems().innerHTML = valor
-    ItensInMyCart().innerHTML = valor
+
+    DOMSelectors.QuantityOfItems.innerHTML = valor
+    DOMSelectors.NumberOfItensInMyCart.innerHTML = valor
 }
 
 const RemoveItens = () => {
     valor--
-    
+
     if (valor <= -1) {
         return valor = 0
     }
 
-    QuantityOfItems().innerHTML = valor
-    ItensInMyCart().innerHTML = valor
-
+    DOMSelectors.QuantityOfItems.innerHTML = valor
+    DOMSelectors.NumberOfItensInMyCart.innerHTML = valor
 }
